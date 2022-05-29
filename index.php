de8d516cec560a4508610c8adfe4f4068c33e185
EvgenyRomanov <e.romanov93@yandex.ru>
2022-05-29 18:48:07 +0300

    оберунл скрипт в функцию

--- index.php
+++ index.php
@@ -1,70 +1,80 @@
 <?php
 
-$result = [];
+/**
+ * @param string $relativePath
+ * @param string[] $popularCrawlers
+ * @return string
+ */
+function parsingAccessLog(string $relativePath, array $popularCrawlers = ['Google', 'Bing', 'Baidu', 'Yandex']): string {
+    $result = [];
 
-$views = 0;
-$urls = [];
-$summaryTraffic = 0;
-$statusCodes = [];
-$crawlers = [];
-$popularCrawlers = ['Google', 'Bing', 'Baidu', 'Yandex'];
+    $views = 0;
+    $urls = [];
+    $summaryTraffic = 0;
+    $statusCodes = [];
+    $crawlers = [];
 
-$file = fopen('test.txt', 'rt');
-
-while (!feof($file)) {
-
-    $string = fgets($file);
-	$pattern = '/(.*?) - - \[(.*?)\] "(.*?)" (\d{1,}) (\d{1,}) "(.*?)" "(.*?)"/is'; 
-
-    preg_match($pattern, $string, $matches);
-
-    if (count($matches) === 8) {
-
-        $ip = $matches[1];
-        $date = $matches[2];
-        $typeRequest = $matches[3];
-        $statusCode = $matches[4];
-        $traffic = $matches[5];
-        $url = $matches[6];
-        $browserData = $matches[7];
+    $file = fopen($relativePath, 'rt');
     
-        $views++;
+    while (!feof($file)) {
     
-        if (!in_array($url, $urls)) {
-            $urls[] = $url;
-        }
+        $string = fgets($file);
+        $pattern = '/(.*?) - - \[(.*?)\] "(.*?)" (\d{1,}) (\d{1,}) "(.*?)" "(.*?)"/is'; 
     
-        $summaryTraffic += $traffic;
+        preg_match($pattern, $string, $matches);
     
-        if (array_key_exists($statusCode, $statusCodes)) {
-            $statusCodes[$statusCode] += 1;
-        } else {
-            $statusCodes[$statusCode] = 1; 
-        }
+        if (count($matches) === 8) {
     
-        foreach ($popularCrawlers as $crawler) {
-            if (!array_key_exists($crawler,$crawlers)) {
-                $crawlers[$crawler] = 0;
+            $ip = $matches[1];
+            $date = $matches[2];
+            $typeRequest = $matches[3];
+            $statusCode = $matches[4];
+            $traffic = $matches[5];
+            $url = $matches[6];
+            $browserData = $matches[7];
+        
+            $views++;
+        
+            if (!in_array($url, $urls)) {
+                $urls[] = $url;
             }
-            if (stripos($browserData, $crawler)) {
-                 $crawlers[$crawler] += 1;
+        
+            $summaryTraffic += $traffic;
+        
+            if (array_key_exists($statusCode, $statusCodes)) {
+                $statusCodes[$statusCode] += 1;
+            } else {
+                $statusCodes[$statusCode] = 1; 
+            }
+        
+            foreach ($popularCrawlers as $crawler) {
+                if (!array_key_exists($crawler,$crawlers)) {
+                    $crawlers[$crawler] = 0;
+                }
+                if (stripos($browserData, $crawler)) {
+                     $crawlers[$crawler] += 1;
+                }
             }
         }
     }
+    
+    fclose($file);
+    
+    $result['views'] = $views;
+    $result['urls'] = count($urls);
+    $result['traffic'] = $summaryTraffic;
+    $result['statusCodes'] = $statusCodes;
+    $result['crawlers'] = $crawlers;
+    
+    $jsonResult = json_encode($result);
+
+    return $jsonResult;
 }
 
-fclose($file);
 
-$result['views'] = $views;
-$result['urls'] = count($urls);
-$result['traffic'] = $summaryTraffic;
-$result['statusCodes'] = $statusCodes;
-$result['crawlers'] = $crawlers;
-
-$jsonResult = json_encode($result);
 
 echo '<pre>';
-echo $jsonResult;
+echo parsingAccessLog('test.txt');
 echo '</pre>';
 
 ?>
